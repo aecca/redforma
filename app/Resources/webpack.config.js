8fdd3f83bab5f7aa4b566837ca8849c2c0540551
Juan Pablo <juanpablocs21@gmail.com>
2016-11-04 00:11:29 -0500

    ft: webpack config prod and dev

--- app/Resources/webpack.config.js
+++ app/Resources/webpack.config.js
@@ -1,19 +1,49 @@
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPluginReturn = require('html-webpack-plugin-return');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+var isProd  = (process.env.NODE_ENV == 'production');
+
+var filename = isProd ? '[name].[hash].js' : '[name].bundle.js';
+var output = __dirname + '/../../web/assets/';
+
+var plugins = [
+    new HtmlWebpackPlugin({
+        filename:'base.html.twig',
+        template: './views/_layouts/base.html.twig',
+        inject: 'body'
+    }),
+    new HtmlWebpackPlugin({
+        filename:'default.html.twig',
+        template: './views/_layouts/default.html.twig',
+        inject: 'body'
+    }),
+    new HtmlWebpackPluginReturn({output:output}),
+    new webpack.optimize.CommonsChunkPlugin('vendor', filename),
+];
+
+if(isProd){
+    plugins.push(new webpack.NoErrorsPlugin());
+    plugins.push(new webpack.optimize.DedupePlugin());
+    plugins.push(new webpack.optimize.UglifyJsPlugin({compress: { warnings: false } }));
+    plugins.push(new ExtractTextPlugin('app.[hash].css', {allChunks: true}));
+}
+
 module.exports = {
-    context : __dirname + '/src',
+    // devtool : isProd ? 'source-map' : 'eval',
+    devtool : isProd ? 'cheap-module-source-map' : 'eval',
     entry   : {
         app     : __dirname + '/src/app.js',
         vendor : ['jquery']
     },
     output  : {
-        path      : __dirname + '/../../web/assets/',
+        path      : output,
         publicPath: '/assets/',
-        filename  : '[name].min.js',
+        filename  : filename,
         chunkFilename: '[id].[name].min.js'
     },
-    plugins: [
-        new webpack.optimize.CommonsChunkPlugin('vendor', '[name].min.js')
-    ],
+    plugins: plugins,
     module: {
         loaders: [
             {
@@ -24,11 +54,11 @@ module.exports = {
             },
             {
                 test: /\.css?$/,
-                loaders: [ 'style','css']
+                loader: isProd ?  ExtractTextPlugin.extract('css-loader') : 'style-loader!css-loader'
             },
             {
                 test: /\.scss$/,
-                loaders: ["style", "css", "sass"]
+                loader: isProd ? ExtractTextPlugin.extract('css-loader!sass-loader') : "style-loader!css-loader!sass-loader"
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|png|jpg)$/,
